42448206ba57ebc3b03968615135f92bfa5012ed
Chris Vinzons <admin@Chriss-MacBook-Pro.local>
2018-05-16 13:08:01 -0500

    Chapter 2 Section 3

--- wip/start/app.js
+++ wip/start/app.js
@@ -1,17 +1,27 @@
 (function() {
   "use strict";
 
-  // Start here
-  (function() {
-    "use strict";
-  
-    var ProductCustomizer = React.createElement(
+  function ProductImage(props) {
+    return React.createElement("img", {
+      src: "../../../assets/red.jpg",
+      alt: "Product Image"
+    });
+  }
+
+  function ProductCustomizer(props) {
+    return React.createElement(
       "div",
       { className: "customizer" },
-      "Product customizer will go here"
+      React.createElement(
+        "div",
+        { className: "product-image" },
+        React.createElement(ProductImage)
+      )
     );
-  
-    ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
-  })();
-  
+  }
+
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById("react-root")
+  );
 })();
